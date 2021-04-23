493e7d6596cd33e7c0a324d61e4b5270eec12e9c
Stewy Slocum <slocumstewy@gmail.com>
2021-04-23 01:11:02 -0400

    calibration started

--- AR-Project/Assets/Scripts/GameScript.cs
+++ AR-Project/Assets/Scripts/GameScript.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class GameScript : MonoBehaviour
 {
-  // Start is called before the first frame update
+  [SerializeField]
+  private ARTrackedImageManager m_TrackedImageManager;
+  private ARSessionOrigin m_SessionOrigin;
+
   void Start()
   {
-
+    m_SessionOrigin = GetComponent<ARSessionOrigin>();
   }
 
-  // Update is called once per frame
+  void OnEnable()
+  {
+    m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    m_TrackedImageManager.trackedImagesChanged += OnChanged;
+  }
+
+  void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
+
+  void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+  {
+    foreach (var newImage in eventArgs.added)
+    {
+      Debug.Log(newImage.transform.position);
+      m_SessionOrigin.transform.position = newImage.transform.position;
+    }
+
+    foreach (var updatedImage in eventArgs.updated)
+    {
+      // Handle updated event
+    }
+
+    foreach (var removedImage in eventArgs.removed)
+    {
+      // Handle removed event
+    }
+  }
+
   void Update()
   {
-
+    //Debug.Log(m_SessionOrigin.transform.position);
   }
 }
