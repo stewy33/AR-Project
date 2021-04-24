ef7fd213e4a063d865a2c56c50ecc12d6db04e68
Jason <codethejason@outlook.com>
2021-04-24 15:26:50 -0400

    added networking and game screen

---
+++ AR-Project/Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+[RequireComponent(typeof(InputField))]
+public class PlayerNameInputField : MonoBehaviour
+{
+    #region Private Constants
+
+    const string playerNamePrefKey = "PlayerName";
+    
+    #endregion
+    #region MonoBehaviour CallBacks
+    // Start is called before the first frame update
+    void Start()
+    {
+        string defaultName = string.Empty;
+        InputField _inputField = this.GetComponent<InputField>();
+        if (_inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+        PhotonNetwork.NickName = defaultName;
+    }
+    #endregion
+
+    #region Public Methods
+    public void SetPlayerName(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Player Name is null or empty");
+            return;
+        }
+        PhotonNetwork.NickName = value;
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+    #endregion
+}
