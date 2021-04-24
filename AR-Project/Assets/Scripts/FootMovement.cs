c5495a2b0d79908cdeb88a3d4d0d89ff4bd20e99
hdesai4204 <harshild24@gmail.com>
2021-04-24 17:56:04 -0400

    changed Photon Transfer to takeover

--- AR-Project/Assets/Scripts/FootMovement.cs
+++ AR-Project/Assets/Scripts/FootMovement.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class FootMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    readonly int force = 7;
+    private Vector3 _startPosition;
+    readonly int force = 5;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _startPosition = _rigidbody.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ResetFoot()
+    {
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.transform.position = _startPosition;
     }
 
     public void forward()
