e58b580de2865583ba846e91406ebd1f5590f1e5
Jason <codethejason@outlook.com>
2021-04-24 16:21:39 -0400

    stewy's commits

---
+++ AR-Project/Assets/Scripts/CalibrateWorld.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CalibrateWorld : MonoBehaviour
+{
+  public GameObject world;
+
+  ARTrackedImageManager m_TrackedImageManager;
+  ARSessionOrigin m_SessionOrigin;
+  bool firstFrame = true;
+
+  void Start()
+  {
+    m_SessionOrigin = GetComponent<ARSessionOrigin>();
+  }
+
+  void OnEnable()
+  {
+    m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    m_TrackedImageManager.trackedImagesChanged += OnChanged;
+  }
+
+  void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
+
+  // On first frame that image marker is detected, set world position so
+  // that the marker is at the center of the world.
+  void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+  {
+    foreach (var updatedImage in eventArgs.updated)
+    {
+      if (firstFrame)
+      {
+        world.transform.position = updatedImage.transform.position;
+        firstFrame = false;
+      }
+    }
+  }
+}
