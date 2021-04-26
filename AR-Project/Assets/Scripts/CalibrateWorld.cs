65a0ea640a3b178d31160ca2972846306a691e6e
Stewy Slocum <slocumstewy@gmail.com>
2021-04-26 13:34:33 -0400

    added foot tracking

--- AR-Project/Assets/Scripts/CalibrateWorld.cs
+++ AR-Project/Assets/Scripts/CalibrateWorld.cs
@@ -6,15 +6,16 @@ using UnityEngine.XR.ARFoundation;
 public class CalibrateWorld : MonoBehaviour
 {
   public GameObject world;
+  public GameObject footPrefab;
 
   ARTrackedImageManager m_TrackedImageManager;
   ARSessionOrigin m_SessionOrigin;
-  // bool firstFrame = true;
 
   void Start()
   {
     m_SessionOrigin = GetComponent<ARSessionOrigin>();
   }
+  List<GameObject> feet = new List<GameObject>();
 
   void OnEnable()
   {
@@ -41,10 +42,33 @@ public class CalibrateWorld : MonoBehaviour
 
   void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
   {
-    if (eventArgs.updated.Count > 0)
+    foreach (var newImage in eventArgs.added)
     {
-      ARTrackedImage trackedImage = eventArgs.updated[0];
-      m_SessionOrigin.MakeContentAppearAt(world.transform, trackedImage.transform.position, trackedImage.transform.localRotation);
+      if (newImage.referenceImage.name == "foot marker")
+      {
+        feet.Add(Instantiate(footPrefab, new Vector3(0, 0, 0), Quaternion.identity));
+      }
+    }
+
+    int feetUpdatedCounter = 0;
+    foreach (var updatedImage in eventArgs.updated)
+    {
+      if (updatedImage.referenceImage.name == "calibration marker")
+      {
+        m_SessionOrigin.MakeContentAppearAt(world.transform, updatedImage.transform.position, updatedImage.transform.localRotation);
+      }
+      if (updatedImage.referenceImage.name == "foot marker")
+      {
+        var foot = feet[feetUpdatedCounter++];
+        foot.transform.position = updatedImage.transform.position;
+        foot.transform.rotation = updatedImage.transform.rotation;
+      }
+    }
+
+    foreach (var removedImage in eventArgs.removed)
+    {
+      Destroy(feet[feet.Count - 1]);
+      feet.RemoveAt(feet.Count - 1);
     }
   }
 }
